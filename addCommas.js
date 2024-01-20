ef25fa9428d4aa26660e2881345e7c94594fb757
Jaleel Whaley <jw430118@gmail.com>
2024-01-20 03:46:00 -0500

    added part 2 and testsmake sure you review to understand

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,26 @@
-function addCommas() {}
+function addCommas(num) {
+    if (isNaN(num))  {
+        return "Must be a Number!"
+    }
+    let number = num.toString();
+    let string = "";
+    let negative = "";
+    for (let i = 0; i < number.length; i++) {
+        if (number.includes("-")) {
+              negative += number[i];
+            number = number.substring(1); 
+            
+          
+            }
+        if (i > 0 && i % 3 === 0) {
+            string = "," + string;
+        }
+        string = number[number.length - 1 - i] + string;
+    }
+    return negative + string;
+}
+//     const numberArray = Array.from(number);
+//    }
+
 
 module.exports = addCommas;
